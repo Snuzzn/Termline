d16644c83f806bdb2a94d02cbf9934acd65c5e73
Sanojan Thiyagaraja <sanojan99@gmail.com>
2021-06-13 23:07:35 +1000

    added pop sound on hover for tasks that have a link

--- src/components/Entry.js
+++ src/components/Entry.js
@@ -1,7 +1,11 @@
 import { Tag, Button } from "@chakra-ui/react";
 import React from "react";
+import hoverPop from "../sounds/hoverPop.mp3";
+import useSound from "use-sound";
 
 export default function Entry({ text, color, link }) {
+  const [play, { stop }] = useSound(hoverPop);
+
   return (
     <a href={link} target="_blank" rel="noopener noreferrer">
       <Tag
@@ -12,6 +16,11 @@ export default function Entry({ text, color, link }) {
         variant="solid"
         colorScheme={color}
         _hover={{ filter: "brightness(110%)" }}
+        onMouseEnter={() => {
+          if (link) {
+            play();
+          }
+        }}
       >
         {text}
       </Tag>
